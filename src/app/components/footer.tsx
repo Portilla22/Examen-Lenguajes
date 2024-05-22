fd520c8f7f13f8bf303ca8d5daa93378c1d96eb0
Josue <Josuealpizar2290@gmail.com>
2024-05-21 19:11:26 -0600

    Sobre ruedas

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>Derechos de autor © 2024 - JeiPiEi</p>
+    </footer>
+  );
+}
+
+export default footer;
